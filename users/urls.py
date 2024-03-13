98456f5060e43d96e85f3a8999127d34e9ef8e8e
deividcuello <deividcuellocuevas@gmail.com>
2024-03-13 07:49:20 -0400

    first commit

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.urls import include, re_path
+from . import views
+
+urlpatterns = [
+	path('register', views.UserRegister.as_view(), name='register'),
+	path('login', views.UserLogin.as_view(), name='login'),
+	path('logout', views.UserLogout.as_view(), name='logout'),
+	path('user', views.UserView.as_view(), name='user'),
+	path('users', views.UsersView.as_view(), name='users'),
+    path('users/<int:username_id>/', views.UserDetailApiView.as_view()),
+]
